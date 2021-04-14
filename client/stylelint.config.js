b6e11f6b07d7172e9ee34c7c92787d21adbd474a
Florian Kapaun <hello@florian-kapaun.de>
2021-04-14 15:28:24 +0200

    :rotating_light: improved linting and data fetching demo

--- client/stylelint.config.js
+++ client/stylelint.config.js
@@ -2,5 +2,14 @@ module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
     // add your custom config here
     // https://stylelint.io/user-guide/configuration
-    rules: {},
+    rules: {
+        indentation: [
+            4,
+            {
+                except: ['block'],
+                message: 'Please use 4 spaces for indentation.',
+                severity: 'warning',
+            },
+        ],
+    },
 }
