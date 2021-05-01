106dfc5fa5421750741eb4497e05ce4dde9d4bbb
Florian Kapaun <hello@florian-kapaun.de>
2021-05-01 18:29:01 +0200

    :hankey: Setup (roughly) Verify Domain Page

---
+++ client/pages/users/verify-domain.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Setup your first domain</h1>
+        <p>
+            Paste the Eco-Web code into the <code>&#60;head&#62;</code> of every
+            page on your domain. And then verify your installation.
+        </p>
+        <button>Copy to clipboard</button>
+        <pre><code>&#60;!-- Eco-Web Tracking Code --&#62;
+&#60;script&#62;(function(e,c,o,w,e,b){e.ew=e.ew||function(){(e.ew.q=e.ew.q||[]).push(arguments)};...})(window,document,’https://eco-website.com/tracking.js’);&#60;/script&#62;</code></pre>
+        <button type="submit" :disabled="getIsLoading">
+            {{ getIsLoading ? 'Loading...' : 'Verify Installation' }}
+        </button>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    layout: 'minimal',
+    middleware: ['auth'],
+    data() {
+        return {
+            siteUrl: undefined,
+            averageMonthlyPageViews: undefined,
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getIsLoading: 'domains/getIsLoading',
+            getAccessTokenExpiry: 'auth/getAccessTokenExpiry',
+        }),
+    },
+    methods: {
+        ...mapActions({
+            registerDomain: 'domains/register',
+        }),
+        async handleSubmit() {},
+    },
+}
+</script>
