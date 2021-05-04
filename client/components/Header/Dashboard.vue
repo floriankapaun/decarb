731fa0987506cdd1f3b6b48bc1a38cb7cca30ba2
Florian Kapaun <hello@florian-kapaun.de>
2021-05-04 14:52:25 +0200

    :lipstick: Improve Dashboard Navigation Style

--- client/components/Header/Dashboard.vue
+++ client/components/Header/Dashboard.vue
@@ -32,6 +32,10 @@
     }
 }
 
+.bx--side-nav__navigation {
+    border-right: 1px solid #e0e0e0;
+}
+
 .bx--header__nav::before {
     background-color: #016752;
 }
--- client/layouts/dashboard.vue
+++ client/layouts/dashboard.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
         <HeaderDashboard />
-        <CvContent id="#main-content" class="bx--grid">
+        <CvContent
+            id="#main-content"
+            class="bx--grid bx--grid--full-width dashboard"
+        >
             <Nuxt />
             <FooterMinimal />
         </CvContent>
     </div>
 </template>
+
+<style lang="scss" scoped>
+// Add padding to body if left-panel is expanded
+@media (min-width: 65.99rem) {
+    .dashboard {
+        padding-left: 18rem;
+    }
+}
+</style>
