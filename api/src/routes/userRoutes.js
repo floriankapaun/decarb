aa4208b150175d5de159d4c4003b7d230c56ab0d
Florian Kapaun <hello@florian-kapaun.de>
2021-05-06 16:35:21 +0200

    :zap: Update userRoutes with Error Handling

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -15,30 +15,32 @@ export default (app) => {
         // Make sure neither password nor verificationCode are leaked
         delete newUser.password;
         delete newUser.verificationCode;
-        sendResponse(res, newUser);
+        return sendResponse(res, newUser);
     }));
 
     // Reset verificationCode: Create new verificationCode and send Mail with new code
-    router.post('/:id/reset-verification', async (req, res) => {
+    router.post('/:id/reset-verification', asyncHandler(async (req, res) => {
         const { id } = req.params;
         // NOTE: This is currently not returning an updatedUser but the nodemail response
         const updatedUser = await UserService.resendVerificationCode(id);
-        res.json(updatedUser).status(200);
-    });
+        return sendResponse(res, updatedUser);
+    }));
 
     // Verify user email
-    router.post('/:id/verification', async (req, res) => {
+    // TODO: Make verification code expire after 30 Minutes
+    router.post('/:id/verification', asyncHandler(async (req, res) => {
         const { id } = req.params;
         const { verificationCode } = req.body;
         const verification = await UserService.verify(id, verificationCode);
-        res.json(verification).status(200);
-    });
+        return sendResponse(res, verification);
+    }));
 
     // Set user password
-    router.post('/:id/password', async (req, res) => {
+    // FIXME: This route is currently working forever and without authentication
+    router.post('/:id/password', asyncHandler(async (req, res) => {
         const { id } = req.params;
         const { password } = req.body;
         const set = await UserService.setUserPassword(id, password);
-        res.json(set).status(200);
-    });
+        return sendResponse(res, set);
+    }));
 };
