3913e2ab89dad36fd8f0365813f59c0cfd94e132
Florian Kapaun <hello@florian-kapaun.de>
2021-05-08 15:43:20 +0200

    :construction: Add Traffic Page Template

---
+++ client/pages/dashboard/traffic.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="bx--row">
+        <div class="bx--col-lg-16">
+            <h1>Traffic</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    layout: 'dashboard',
+    computed: {
+        ...mapGetters({
+            getUser: 'auth/getUser',
+            getSelectedDomain: 'domains/getSelectedDomain',
+        }),
+    },
+    async mounted() {
+        await this.fetchUserDomains(this.getUser.id)
+    },
+    methods: {
+        ...mapActions({}),
+    },
+}
+</script>
