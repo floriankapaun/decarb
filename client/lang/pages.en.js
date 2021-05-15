70fc9ef93cc1187540ddb81e1969dd4ade16d5f2
Florian Kapaun <hello@florian-kapaun.de>
2021-05-15 18:45:40 +0200

    :speech_balloon: Internationalize Register Page

--- client/lang/pages.en.js
+++ client/lang/pages.en.js
@@ -1,5 +1,6 @@
 import { en as index } from './pages/index'
 import { en as about } from './pages/about'
 import { en as login } from './pages/login'
+import { en as register } from './pages/register'
 
-export default { index, about, login }
+export default { index, about, login, register }
