d4e7d2b6391c354f753700f3648b820bc666a5e3
Florian Kapaun <hello@florian-kapaun.de>
2021-05-15 19:09:42 +0200

    :speech_balloon: Internationalize Legal Pages

--- client/lang/pages.en.js
+++ client/lang/pages.en.js
@@ -1,13 +1,17 @@
-import { en as index } from './pages/index'
 import { en as about } from './pages/about'
+import { en as dataPrivacy } from './pages/legal/dataPrivacy'
+import { en as imprint } from './pages/legal/imprint'
+import { en as index } from './pages/index'
 import { en as login } from './pages/login'
 import { en as register } from './pages/register'
 import { en as setPassword } from './pages/users/id/setPassword'
+import { en as termsAndConditions } from './pages/legal/termsAndConditions'
 import { en as verifyEmail } from './pages/users/id/verifyEmail'
 
 export default {
     index,
     about,
+    legal: { dataPrivacy, imprint, termsAndConditions },
     login,
     register,
     users: { id: { setPassword, verifyEmail } },
--- client/pages/legal/imprint.vue
+++ client/pages/legal/imprint.vue
@@ -3,7 +3,7 @@
         <div
             class="bx--col-sm-4 bx--offset-md-2 bx--col-md-4 bx--col-lg-8 bx--offset-xlg-6 bx--col-xlg-4"
         >
-            <h1>Imprint</h1>
+            <h1>{{ $t('p.legal.imprint.h1') }}</h1>
         </div>
     </section>
 </template>
