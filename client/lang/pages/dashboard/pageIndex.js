a1373beb989d3c35a957d067292dbafbc797424a
Florian Kapaun <hello@florian-kapaun.de>
2021-05-16 22:52:24 +0200

    :children_crossing: Add Search, Sort and Pagination to PageIndex Table (Client-Side)

--- client/lang/pages/dashboard/pageIndex.js
+++ client/lang/pages/dashboard/pageIndex.js
@@ -1,5 +1,8 @@
 export const en = {
     h1: 'Page Index',
+    title: 'Known pages',
+    helperText:
+        'If you want to index a new page, just visit it with your browser.',
     columns: {
         url: 'URL',
         createdAt: 'Registered Since',
