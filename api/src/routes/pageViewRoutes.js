5e67feb4fb405721690c7c4253e7e40a33895cdd
Florian Kapaun <hello@florian-kapaun.de>
2021-05-20 16:38:55 +0200

    :alien: Update PageView Route to work with urlencoded data

--- api/src/routes/pageViewRoutes.js
+++ api/src/routes/pageViewRoutes.js
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 import PageViewService from '../services/PageViewService.js';
@@ -6,15 +6,13 @@ import AppError from '../utils/AppError.js';
 import asyncHandler from '../utils/asyncHandler.js';
 import sendResponse from '../utils/sendResponse.js';
 
-const router = Router();
+const router = express.Router();
 
 export default (app) => {
     app.use('/pageviews', router);
 
-    router.options('/', cors()) // enable pre-flight request
-
     // Register a pageView
-    router.post('/', cors(), asyncHandler(async (req, res) => {
+    router.post('/', cors(), express.urlencoded({ extended: false }), asyncHandler(async (req, res) => {
         const origin = req.get('Origin');
         if (!origin) throw new AppError(`No origin provided`, 400);
         const { body } = req;
