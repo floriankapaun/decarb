661f0af3e1762b8e5089a0edcbce40c6d9af6fed
floriankapaun <fk@boxbote.de>
2021-06-02 18:12:35 +0200

    :construction: WIP (Switching Device)

---
+++ client/pages/dashboard/badge.vue
@@ -0,0 +1,71 @@
+<template>
+    <section class="bx--row">
+        <div class="bx--col-lg-16">
+            <h1>{{ $t('p.dashboard.index.h1') }}</h1>
+        </div>
+        <div class="bx--col-lg-8 col--card">
+            <Card headline="Achievements">
+                <p>Wow ey!</p>
+            </Card>
+        </div>
+        <div class="bx--col-lg-8 col--card">
+            <Card headline="Pageviews">
+                <p>Wow ey!</p>
+            </Card>
+        </div>
+        <div class="bx--col-lg-8 col--card">
+            <Card headline="Emission Development">
+                <p>Wow ey!</p>
+            </Card>
+        </div>
+    </section>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    layout: 'dashboard',
+    nuxtI18n: {
+        paths: {
+            en: '/dashboard',
+        },
+    },
+    computed: {
+        ...mapGetters({
+            getAccessToken: 'auth/getAccessToken',
+            getUser: 'auth/getUser',
+            getSelectedDomain: 'domains/getSelectedDomain',
+        }),
+    },
+    async mounted() {
+        if (!this.getUser) {
+            if (!this.getAccessToken) {
+                // TODO: Remove that stuff...
+                console.warn('No Access Token provided')
+                return false
+            }
+            await this.fetchUser(this.getAccessToken)
+        }
+        if (!this.getSelectedDomain) {
+            await this.fetchUserDomains(this.getUser.id)
+        }
+    },
+    methods: {
+        ...mapActions({
+            fetchUser: 'auth/fetchUser',
+            fetchUserDomains: 'domains/fetchUserDomains',
+        }),
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    margin-bottom: $spacing-07;
+}
+
+.col--card {
+    margin-bottom: $spacing-07;
+}
+</style>
