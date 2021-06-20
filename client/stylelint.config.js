0f34b79456fd0fa05187fc61797564c62bcf9544
Florian Kapaun <hello@florian-kapaun.de>
2021-06-20 14:33:14 +0200

    :rotating_light: Update Linting and Fix Console Warnings

--- client/stylelint.config.js
+++ client/stylelint.config.js
@@ -3,14 +3,7 @@ module.exports = {
     // add your custom config here
     // https://stylelint.io/user-guide/configuration
     rules: {
-        indentation: [
-            4,
-            {
-                except: ['block'],
-                message: 'Please use 4 spaces for indentation.',
-                severity: 'warning',
-            },
-        ],
+        indentation: [4],
         'at-rule-no-unknown': [
             true,
             {
