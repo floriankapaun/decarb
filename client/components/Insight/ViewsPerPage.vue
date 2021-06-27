36000dd8046e3cd7f62b98bbbe457607bafc57ad
Florian Kapaun <hello@florian-kapaun.de>
2021-06-27 15:37:18 +0200

    :building_construction: Build Components for ViewsPerDay and ViewsPerPage

---
+++ client/components/Insight/ViewsPerPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>ViewsPerPage {{ viewsPerPage }}</section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    async fetch() {
+        const store = this.$store
+        if (store.getters['traffic/getViewsPerPage']) return
+
+        // Make sure that selectedDomain exists
+        if (!store.getters['domains/getSelectedDomain']) {
+            if (!store.getters['auth/getUser']) {
+                await store.dispatch('auth/fetchUser')
+            }
+            await store.dispatch(
+                'domains/fetchUserDomains',
+                store.getters['auth/getUser'].id
+            )
+        }
+
+        // Create start and end date for statistics
+        const d = new Date()
+        const year = d.getFullYear().toString().padStart(4, '0')
+        const month = (d.getMonth() + 1).toString().padStart(2, '0')
+        const previousMonth = month - 1
+        const day = d.getDate().toString().padStart(2, '0')
+        const hour = d.getHours().toString().padStart(2, '0')
+        const minute = d.getMinutes().toString().padStart(2, '0')
+        const second = d.getSeconds().toString().padStart(2, '0')
+
+        const now = `${year}-${month}-${day} ${hour}:${minute}:${second}`
+        const monthAgo = `${year}-${previousMonth}-${day} ${hour}:${minute}:${second}`
+
+        // Fetch statistical data
+        if (!store.getters['traffic/getViewsPerPage']) {
+            await store.dispatch('traffic/fetchViewsPerPage', {
+                domainId: store.getters['domains/getSelectedDomain'].id,
+                options: {
+                    timeStart: monthAgo,
+                    timeEnd: now,
+                },
+            })
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getViewsPerPage: 'traffic/getViewsPerPage',
+        }),
+        viewsPerPage() {
+            return this.getViewsPerPage
+        },
+    },
+}
+</script>
--- client/pages/dashboard/traffic.vue
+++ client/pages/dashboard/traffic.vue
@@ -3,82 +3,20 @@
         <div class="bx--row">
             <div class="bx--col-sm-4 p-mb">
                 <h1>{{ $t('p.dashboard.traffic.h1') }}</h1>
-                {{ viewsPerDay }}
+                <InsightViewsPerDay />
+                <InsightViewsPerPage />
             </div>
         </div>
-        {{ viewsPerPage }}
     </section>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
     name: 'DashboardTraffic',
     layout: 'dashboard',
     nuxtI18n: {
         paths: {
             en: '/dashboard/traffic',
-        },
-    },
-    async fetch({ store }) {
-        if (
-            store.getters['traffic/getViewsPerPage'] &&
-            store.getters['traffic/getViewsPerDay']
-        ) {
-            return
-        }
-
-        // Make sure that selectedDomain exists
-        if (!store.getters['domains/getSelectedDomain']) {
-            if (!store.getters['auth/getUser']) {
-                await store.dispatch('auth/fetchUser')
-            }
-            await store.dispatch(
-                'domains/fetchUserDomains',
-                store.getters['auth/getUser'].id
-            )
-        }
-
-        // Create start and end date for statistics
-        const d = new Date()
-        const year = d.getFullYear().toString().padStart(4, '0')
-        const month = (d.getMonth() + 1).toString().padStart(2, '0')
-        const previousMonth = month - 1
-        const day = d.getDate().toString().padStart(2, '0')
-        const hour = d.getHours().toString().padStart(2, '0')
-        const minute = d.getMinutes().toString().padStart(2, '0')
-        const second = d.getSeconds().toString().padStart(2, '0')
-
-        const now = `${year}-${month}-${day} ${hour}:${minute}:${second}`
-        const monthAgo = `${year}-${previousMonth}-${day} ${hour}:${minute}:${second}`
-
-        const params = {
-            domainId: store.getters['domains/getSelectedDomain'].id,
-            options: {
-                timeStart: monthAgo,
-                timeEnd: now,
-            },
-        }
-
-        // Fetch statistical data
-        if (!store.getters['traffic/getViewsPerPage']) {
-            await store.dispatch('traffic/fetchViewsPerPage', params)
-        }
-        if (!store.getters['traffic/getViewsPerDay']) {
-            await store.dispatch('traffic/fetchViewsPerDay', params)
-        }
-    },
-    computed: {
-        ...mapGetters({
-            getViewsPerDay: 'traffic/getViewsPerDay',
-            getViewsPerPage: 'traffic/getViewsPerPage',
-        }),
-        viewsPerDay() {
-            return this.getViewsPerDay
-        },
-        viewsPerPage() {
-            return this.getViewsPerPage
         },
     },
 }
