f297c51d7c5fa4c629409cb4dbe11a9be09bf1b7
Florian Kapaun <hello@florian-kapaun.de>
2021-06-27 15:30:18 +0200

    :building_construction: Build Component for EmissionAggregation

---
+++ client/components/Insight/EmissionsAggregation.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>EmissionAggregation {{ aggregation }}</section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    async fetch() {
+        const store = this.$store
+        if (store.getters['emissions/getAggregation']) return
+
+        // Make sure that selectedDomain exists
+        if (!store.getters['domains/getSelectedDomain']) {
+            if (!store.getters['auth/getUser']) {
+                await store.dispatch('auth/fetchUser')
+            }
+            await store.dispatch(
+                'domains/fetchUserDomains',
+                store.getters['auth/getUser'].id
+            )
+        }
+
+        // Fetch statistical data
+        if (!store.getters['emissions/getAggregation']) {
+            // Create start and end date for aggregation
+            const d = new Date()
+            const year = d.getFullYear().toString().padStart(4, '0')
+            const month = (d.getMonth() + 1).toString().padStart(2, '0')
+            const previousMonth = month - 1
+            const day = d.getDate().toString().padStart(2, '0')
+            const hour = d.getHours().toString().padStart(2, '0')
+            const minute = d.getMinutes().toString().padStart(2, '0')
+            const second = d.getSeconds().toString().padStart(2, '0')
+
+            const now = `${year}-${month}-${day} ${hour}:${minute}:${second}`
+            const monthAgo = `${year}-${previousMonth}-${day} ${hour}:${minute}:${second}`
+
+            await store.dispatch('emissions/fetchAggregation', {
+                domainId: store.getters['domains/getSelectedDomain'].id,
+                options: {
+                    timeStart: monthAgo,
+                    timeEnd: now,
+                },
+            })
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getAggregation: 'emissions/getAggregation',
+        }),
+        aggregation() {
+            return this.getAggregation
+        },
+    },
+}
+</script>
--- client/pages/dashboard/emissions.vue
+++ client/pages/dashboard/emissions.vue
@@ -3,7 +3,7 @@
         <div class="bx--row">
             <div class="bx--col-sm-4 p-mb">
                 <h1>{{ $t('p.dashboard.emissions.h1') }}</h1>
-                {{ aggregation }}
+                <InsightEmissionsAggregation />
             </div>
         </div>
         {{ hostingEmissions }}
@@ -22,12 +22,7 @@ export default {
         },
     },
     async fetch({ store }) {
-        if (
-            store.getters['emissions/getHostingEmissions'] &&
-            store.getters['emissions/getAggregation']
-        ) {
-            return
-        }
+        if (store.getters['emissions/getHostingEmissions']) return
 
         // Make sure that selectedDomain exists
         if (!store.getters['domains/getSelectedDomain']) {
@@ -40,43 +35,18 @@ export default {
             )
         }
 
-        const domainId = store.getters['domains/getSelectedDomain'].id
-
         // Fetch statistical data
         if (!store.getters['emissions/getHostingEmissions']) {
-            await store.dispatch('emissions/fetchHostingEmissions', domainId)
-        }
-        if (!store.getters['emissions/getAggregation']) {
-            // Create start and end date for aggregation
-            const d = new Date()
-            const year = d.getFullYear().toString().padStart(4, '0')
-            const month = (d.getMonth() + 1).toString().padStart(2, '0')
-            const previousMonth = month - 1
-            const day = d.getDate().toString().padStart(2, '0')
-            const hour = d.getHours().toString().padStart(2, '0')
-            const minute = d.getMinutes().toString().padStart(2, '0')
-            const second = d.getSeconds().toString().padStart(2, '0')
-
-            const now = `${year}-${month}-${day} ${hour}:${minute}:${second}`
-            const monthAgo = `${year}-${previousMonth}-${day} ${hour}:${minute}:${second}`
-
-            await store.dispatch('emissions/fetchAggregation', {
-                domainId,
-                options: {
-                    timeStart: monthAgo,
-                    timeEnd: now,
-                },
-            })
+            await store.dispatch(
+                'emissions/fetchHostingEmissions',
+                store.getters['domains/getSelectedDomain'].id
+            )
         }
     },
     computed: {
         ...mapGetters({
-            getAggregation: 'emissions/getAggregation',
             getHostingEmissions: 'emissions/getHostingEmissions',
         }),
-        aggregation() {
-            return this.getAggregation
-        },
         hostingEmissions() {
             return this.getHostingEmissions
         },
