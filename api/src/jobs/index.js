b15282ee34f6cda6f538a80d1e833b287d28e12f
Florian Kapaun <hello@florian-kapaun.de>
2021-06-30 19:36:44 +0200

    :bulb: Add ToDo

--- api/src/jobs/index.js
+++ api/src/jobs/index.js
@@ -9,6 +9,9 @@ const jobs = [
         path: './src/jobs/removeUnverifiedUsers.js',
         cron: '30 02 * * *', // Every day at 02:30
     },
+    // TODO: Implement Job to remove unverified Domains
+    // Therefore Upgrade Prisma to get access to the new Cascading logic
+    // See: https://github.com/prisma/prisma/releases/tag/2.26.0
 ];
 
 export default jobs;
