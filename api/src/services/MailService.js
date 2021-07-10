6c9dc08f1c2f16fcc7ddb5056f481670142811f2
Florian Kapaun <hello@florian-kapaun.de>
2021-07-10 16:08:51 +0200

    :recycle: Refactor MailService

--- api/src/services/MailService.js
+++ api/src/services/MailService.js
@@ -9,7 +9,12 @@ import {
     PLATFORM_EMAIL_USER,
 } from '../config/index.js';
 
+
+/**
+ * Handles mail sending
+ */
 class MailService {
+
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: PLATFORM_EMAIL_HOST,
@@ -22,23 +27,20 @@ class MailService {
         });
     }
 
+
     /**
-     * Send a Mail with nodemailer
+     * Send a Mail with Nodemailer
      * 
-     * @param {String} to - comma seperated list of receivers
      * @param {String} subject - mail subject line
      * @param {String} html - mail body
+     * @param {String} to - comma seperated list of receivers
+     * @param {String} from
+     * @returns {Object} - Nodemailer response
      */
-    async send(subject, html, to = ADMIN_EMAIL) {
-        const mailOptions = {
-            from: PLATFORM_EMAIL_SENDER,
-            to: to,
-            subject: subject,
-            html: html,
-        };
-
+    async send(subject, html, to = ADMIN_EMAIL, from = PLATFORM_EMAIL_SENDER) {
+        const mailOptions = { from, to, subject, html };
         const response = await this.transporter.sendMail(mailOptions);
-        // TODO: Add error handling
+        // TODO: Add error handling and don't return the full Nodemailer response
         return response;
     }
 }
