7969c65f27522767876ea6dd083d6e0919daa25f
Florian Kapaun <hello@florian-kapaun.de>
2021-07-10 12:32:40 +0200

    :art: Improve Seeding (Domains)

--- api/src/prisma/seed-data/domains.js
+++ api/src/prisma/seed-data/domains.js
@@ -10,6 +10,8 @@ for (let i = 0; i < config.numberOfDomains; i++) {
         url: faker.internet.domainName(),
         companyName: faker.company.companyName(),
         estimatedMonthlyPageViews: faker.datatype.number(),
+        createdAt: new Date(config.minDatetime),
+        verifiedAt: new Date(config.minDatetime),
     }
     domains.push(fakeDomain)
 }
